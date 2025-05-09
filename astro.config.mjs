497ad915b8384e65ad3ea4f9f50f3c18eb6f15e0
DrabantBY <yauheni.basalai@gmail.com>
2025-05-09 18:48:10 +0300

    feat(common): add astro font feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Montserrat",
+      cssVariable: "--font-montserrat",
+      weights: [400, 500, 600, 700],
+    }]
+  }
+});
